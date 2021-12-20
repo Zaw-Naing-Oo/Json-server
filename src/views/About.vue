19ad79dc684e316dcf50ebb1732fce1ef4a8b80f
Zaw-Naing-Oo <neymarlay553@gmail.com>
2021-12-20 14:23:42 +0630

    first realWorld

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>A Better World for better code</h1>
   </div>
 </template>
---
+++ src/views/EventList.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="events">
+    <h1>Event For Good</h1>
+    <EventCard  v-for="event in events" :key="event.id" :event='event' />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+
+   data () {
+    return {
+      events : [ 
+
+       { 
+         id : 5555555,
+        category : 'animal welfare',
+        title : 'Cat abdotion Day',
+        description : 'Find your new feline friend at this event',
+        location : 'yangon',
+        date : 'january 28, 2022',
+        time : '12:00',
+        petsAllowed : true,
+        origanizer  : 'Zaw Naing Oo'
+        },
+        { 
+         id : 8888888,
+        category : 'animal welfare',
+        title : 'Rabbit abdotion Day',
+        description : 'Find your new feline friend at this event',
+        location : 'yangon',
+        date : 'january 28, 2022',
+        time : '12:00',
+        petsAllowed : true,
+        origanizer  : 'Zaw Naing Oo'
+        },
+        { 
+         id : 9999999,
+        category : 'animal welfare',
+        title : 'Dog abdotion Day',
+        description : 'Find your new feline friend at this event',
+        location : 'yangon',
+        date : 'january 28, 2022',
+        time : '12:00',
+        petsAllowed : true,
+        origanizer  : 'Zaw Naing Oo'
+        }
+
+       ]
+    }
+ }
+
+
+};
+</script>
+
+<style scoped>
+
+.events{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
